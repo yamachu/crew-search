929c8920e0cd0ab8f4d5f0349684151fce7e3441
yamachu <yamachu.dev@gmail.com>
2018-11-11 01:08:55 +0900

    これぐらいのViewの大きさで足りそう

--- src/renderers/styles/global.ts
+++ src/renderers/styles/global.ts
@@ -3,7 +3,9 @@ import { createGlobalStyle } from 'styled-components';
 const globalStyle = createGlobalStyle`
     body {
         margin: 0;
+        width: 400px;
+        height: 400px;
     }
-`; // サイズとか
+`;
 
 export default globalStyle;
