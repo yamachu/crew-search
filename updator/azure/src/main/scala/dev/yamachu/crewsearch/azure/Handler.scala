3ebbfa020fd69e080defce8e8e0b787cb606539f
yamachu <yamachu.dev@gmail.com>
2019-04-07 01:37:57 +0900

    Handlerでemailの確認を行う

--- updator/azure/src/main/scala/dev/yamachu/crewsearch/azure/Handler.scala
+++ updator/azure/src/main/scala/dev/yamachu/crewsearch/azure/Handler.scala
@@ -5,6 +5,7 @@ import com.microsoft.azure.functions.annotation._
 import dev.yamachu.crewsearch._
 import dev.yamachu.crewsearch.azure.utils.Logger
 import dev.yamachu.crewsearch.objects.Requests
+import dev.yamachu.crewsearch.services.MockFirebaseAuthService
 
 import collection.JavaConverters._
 
@@ -18,13 +19,20 @@ class Handler {
                   context: ExecutionContext): HttpResponseMessage = {
     val logger = Logger(context.getLogger)
     logger.info("Scala HTTP POST trigger processed a request.")
+
     val maybeFirebaseToken =
       request.getHeaders.asScala.get("Authorization").map(_.replace("Bearer ", ""))
+    val body = request.getBody
 
     (for {
       firebaseToken <- maybeFirebaseToken.toRight(new Exception("Cannot find Authorization header"))
-      // ここでFirebaseToken使ってemailアドレスのValidation
-      result        <- Functions(logger).run(request.getBody)
+      _ <- new MockFirebaseAuthService()
+        .verifyEmail(firebaseToken, body.email)
+        .flatMap(
+          if (_) Right(())
+          else Left(new Exception("request email do not match firebase user"))
+        )
+      result <- Functions(logger).run(body)
     } yield result) match {
       case Right(s) =>
         request.createResponseBuilder(HttpStatus.OK).body(s).build
