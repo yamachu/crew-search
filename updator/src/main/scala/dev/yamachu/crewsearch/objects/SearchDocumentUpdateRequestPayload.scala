3d718df0eb1a178fa08464c367b2f0de02af1d64
yamachu <yamachu.dev@gmail.com>
2019-04-07 16:44:11 +0900

    Serializer辺りがバグってた

--- updator/src/main/scala/dev/yamachu/crewsearch/objects/SearchDocumentUpdateRequestPayload.scala
+++ updator/src/main/scala/dev/yamachu/crewsearch/objects/SearchDocumentUpdateRequestPayload.scala
@@ -1,14 +1,32 @@
 package dev.yamachu.crewsearch.objects
 
 import dev.yamachu.crewsearch.utils._
-import io.circe.Encoder
+import io.circe.{Encoder, Json, KeyEncoder}
 import io.circe.generic.auto._
 import io.circe.syntax._
 
-case class SearchDocumentRequestPayload(key: String, `@search.action`: SearchAction) {
+case class SearchDocumentRequestPayload(__indexKey: IndexKey, `@search.action`: SearchAction) {
   def addBody(requests: AnyRef): Map[String, String] = (ccToMap(this) ++ ccToMap(requests)).map {
-    case (k, v) => k -> v.toString
+    case (_, v) if v.isInstanceOf[SearchAction] => "@search.action" -> v.toString
+    case (k, _) if k == "__indexKey"            => __indexKey.key   -> __indexKey.value.toString
+    case (k, v)                                 => k                -> v.toString
   }
+
+}
+
+object SearchDocumentRequestPayload {
+  implicit val encoder: Encoder[SearchDocumentRequestPayload] = (a: SearchDocumentRequestPayload) =>
+    Json.obj(
+      (a.__indexKey.key, Json.fromString(a.__indexKey.value.toString)),
+      ("@search.action", Json.fromString(a.`@search.action`.toString))
+  )
+}
+
+case class IndexKey(key: String, value: String)
+
+object IndexKey {
+  implicit val keyEncoder: KeyEncoder[IndexKey] = (ik: IndexKey) => ik.key
+  implicit val encoder: Encoder[IndexKey]       = (ik: IndexKey) => ik.value.asJson
 }
 
 sealed trait SearchDocumentUpdateRequestTrait
@@ -29,32 +47,39 @@ object SearchDocumentUpdateRequestTrait {
 
 object SearchDocumentRequest {
   def upload[T <: AnyRef](requests: Seq[T],
-                          keySelector: T => String): SearchDocumentUpdateRequestTrait =
+                          keySelector: T => IndexKey): SearchDocumentUpdateRequestTrait =
     SearchDocumentUpdateRequestMap(
       value = requests.map(r => SearchDocumentRequestPayload(keySelector(r), Upload).addBody(r))
     )
 
   def merge[T <: AnyRef](requests: Seq[T],
-                         keySelector: T => String): SearchDocumentUpdateRequestTrait =
+                         keySelector: T => IndexKey): SearchDocumentUpdateRequestTrait =
     SearchDocumentUpdateRequestMap(
       value = requests.map(r => SearchDocumentRequestPayload(keySelector(r), Merge).addBody(r))
     )
 
   def mergeOrUpload[T <: AnyRef](requests: Seq[T],
-                                 keySelector: T => String): SearchDocumentUpdateRequestTrait =
+                                 keySelector: T => IndexKey): SearchDocumentUpdateRequestTrait =
     SearchDocumentUpdateRequestMap(
       value =
         requests.map(r => SearchDocumentRequestPayload(keySelector(r), MergeOrUpload).addBody(r))
     )
 
   def delete[T <: AnyRef](requests: Seq[T],
-                          keySelector: T => String): SearchDocumentUpdateRequestTrait =
+                          keySelector: T => IndexKey): SearchDocumentUpdateRequestTrait =
     SearchDocumentUpdateRequest(
       value = requests.map(r => SearchDocumentRequestPayload(keySelector(r), Delete))
     )
 }
 
-sealed trait SearchAction
+sealed trait SearchAction {
+  override def toString: String = this match {
+    case Upload        => "upload"
+    case Merge         => "merge"
+    case MergeOrUpload => "mergeOrUpload"
+    case Delete        => "delete"
+  }
+}
 
 object SearchAction {
   implicit val encode: Encoder[SearchAction] = Encoder.instance {
@@ -63,6 +88,8 @@ object SearchAction {
     case _ @MergeOrUpload => "mergeOrUpload".asJson
     case _ @Delete        => "delete".asJson
   }
+
+  implicit val keyEncoder: KeyEncoder[SearchAction] = _ => "@search.action"
 }
 
 case object Upload extends SearchAction
