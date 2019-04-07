686e34430c81a4ea050d586d56f702f9826ceb5b
yamachu <yamachu.dev@gmail.com>
2019-04-07 14:49:46 +0900

    実際に叩ける状態にする

--- updator/src/main/scala/dev/yamachu/crewsearch/Functions.scala
+++ updator/src/main/scala/dev/yamachu/crewsearch/Functions.scala
@@ -1,14 +1,16 @@
 package dev.yamachu.crewsearch
 
 import dev.yamachu.crewsearch.objects._
+import dev.yamachu.crewsearch.services.AzureSearchService
 
 case class Functions(logger: utils.Logger) {
   def run(req: Requests): Either[Throwable, Response] = {
     logger.info("This is common Functions run")
-    req.name match {
-      case "yamachu" => Right(Response())
-      case _         => Left(new Exception(s"require admin user name, but ${req.name}"))
-    }
+    for {
+      _ <- AzureSearchService.instance
+        .updateField(req.email,
+                     SearchDocumentRequest.mergeOrUpload[Requests](Seq(req), s => s.email))
+    } yield Response()
   }
 
 }
